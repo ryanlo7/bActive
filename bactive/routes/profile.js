8bba6fe73d6e8329f21442692f9e7cb2a2c3d891
NKCWong <n.kc.wong.123@gmail.com>
2018-11-14 04:37:07 +0000

    Basic profile display

--- bactive/routes/profile.js
+++ bactive/routes/profile.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('This is the profile page. Users can view their or other users profiles here');
+/* GET user information. */
+router.get('/:userid', function(req, res, next) {
+	var db = req.app.locals.db;
+	var userId = parseInt(req.params.userid);
+
+	db.collection('Users')
+			.find({'userId': userId})
+			.toArray(function(err, results) {
+				if (results.length == 0) {
+					res.status(404).send("404: userId not found");
+				} else {
+					user = results[0]; // should only be one match
+
+					res.render('profile', {
+					 	userId: userId,
+						email: user.email,
+						activities: user.activities,
+						availability: user.availability
+					});
+				}
+			});
 });
 
 module.exports = router;
