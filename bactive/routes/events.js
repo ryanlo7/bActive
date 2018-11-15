23b3b53a808c9b12a41e264a092ba593b3cfe868
Krishna Babu <kbabu@g.ucla.edu>
2018-11-15 15:12:20 -0800

    functional events page

--- bactive/routes/events.js
+++ bactive/routes/events.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('This is the events page. User views their upcoming events');
+router.get('/:userid', function(req, res, next) {
+	var db = req.app.locals.db; //get instance of db
+	var userId = parseInt(req.params.userid);
+
+	db.collection('Users')
+			.find({'userId': userId})
+			.toArray(function(err, results) {
+				if (results.length == 0) {
+					res.status(404).send("404: userId not found");
+				} else {
+					user = results[0]; // should only be one match
+
+					res.render('events', {
+					 	userId: userId,
+						email: user.email,
+						activities: user.activities,
+						availability: user.availability,
+						events: user.events
+					});
+				}
+			});
+
 });
 
 module.exports = router;
