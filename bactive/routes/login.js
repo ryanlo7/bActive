fa328d257c757dc22bfde08ecf12a4db7cc13f17
elkong <erickongl@gmail.com>
2018-11-15 17:48:09 -0800

    Added documentation for functions in login.js.

--- bactive/routes/login.js
+++ bactive/routes/login.js
@@ -3,11 +3,32 @@ var router = express.Router();
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
-router.get('/', function(req, res) {
+router.get('/',
+	/**
+		* Function to handle GET requests for login page.
+		* Render the registration page, and return it through res.
+		* @param {Object} req The express routing HTTP client request object.
+		* @param {Object} res The express routing HTTP client response object.
+		* @return {Object} A JSON object that holds req and res.
+	*/
+	function(req, res) {
 	res.render('login', {err: null});
 });
 
-router.post('/', function(req, res) {
+router.post('/',
+	/**
+		* Function to handle POST requests for login page.
+		* Causes the server to return 400 error if malformed request.
+		* Queries MongoDB for this user. If it doesn't exist, or the password doesn't match
+		* with cryptographic hash, then causes the server to return a 401 error, and redirects to login.
+		* If all goes well, then redirects user to her profile page
+		* with a JSON web token that lasts two hours.
+		* @param {Object} req The express routing HTTP client request object,
+		* whose body contains the user's email and password.
+		* @param {Object} res The express routing HTTP client response object.
+		* @return {Object} A JSON object that holds req and res.
+	*/
+	function(req, res) {
 	var usr, pw;
 	if (req.body.email === undefined || req.body.password === undefined) {
 		res.status(400).send('Bad request');
