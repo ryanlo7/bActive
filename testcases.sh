506338d898587676c7b4c45235e3c76b67314505
eddiehuang97 <eddiehuang97@github.com>
2018-11-15 20:00:21 -0800

    minor error fix to curl testcases

--- testcases.sh
+++ testcases.sh
@@ -1,9 +1,8 @@
 curl -X GET \
   http://localhost:3000/login \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'Postman-Token: 514c45b9-57d0-4907-b840-b834949b9dee' \
-  -H 'cache-control: no-cache' \
-  -d 'username=user1%40ucla.edu&password=incorrect'
+  -H 'Postman-Token: 5b45f9e9-1e9f-495a-8265-d7737ddca90e' \
+  -H 'cache-control: no-cache'
   
 curl -X POST \
   http://localhost:3000/login \
