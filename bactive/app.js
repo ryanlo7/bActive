6baf512e26761ffa8c60f20176f4b2ebdcb0be9c
NKCWong <n.kc.wong.123@gmail.com>
2018-11-16 00:04:58 +0000

    Fix logins and integrate with profile

--- bactive/app.js
+++ bactive/app.js
@@ -44,69 +44,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // connect the /routes/*.js files to the url here:
 app.use('/', indexRouter);
-// app.use('/login', loginRouter);
+app.use('/login', loginRouter);
 app.use('/register', registerRouter);
-// app.use('/profile', profileRouter);
+app.use('/profile', profileRouter);
 app.use('/match', matchRouter);
 app.use('/events', eventsRouter);
 app.use('/edit', editRouter);
-
-app.get('/login', function(req, res) {
-	res.render('login', {err: null});
-});
-
-app.post('/login', function(req, res) {
-	var usr, pw;
-	if (req.body.email === undefined || req.body.password === undefined) {
-		res.status(400).send('Bad request');
-		return;
-	}
-	usr = req.body.email;
-	pw = req.body.password;
-	var query = {email: usr};
-	app.locals.db.collection('Users').findOne(query, function(err, result) {
-		if(result === null) {
-			res.status(401).render('login', {err: 'Invalid email/password combination'});
-			return;
-		}
-		bcrypt.compare(pw, result.password, function(err, rs) {
-			if(rs == true) {
-				var payload = {"exp": Math.floor(Date.now() / 1000) + (2 * 60 * 60), "usr": usr};
-				var header = {"alg": "HS256", "typ": "JWT"};
-				var cert = "C-UFRaksvPKhx1txJYFcut3QGxsafPmwCY6SCly3G6c";
-				jwt.sign(payload, cert, { algorithm: 'HS256',  header: header}, function(err, token) {
-					res.cookie("jwt", token, {expires: 0});
-					res.redirect('profile');
-				});
-			}
-			else {
-				res.status(401).render('login', {err: 'Invalid email/password combination'});
-			}
-		});
-	});
-});
-
-app.get('/profile', function(req, res) {
-	var cert = "C-UFRaksvPKhx1txJYFcut3QGxsafPmwCY6SCly3G6c";
-	if(req.cookies.jwt === undefined) {
-		res.status(401);
-		res.render('login', {});
-		return;
-	}
-	jwt.verify(req.cookies.jwt, cert, {algorithms: ["HS256"]}, function(err, decoded) {
-		if(decoded === undefined) {
-		res.status(401);
-		res.render('login', {});
-			return;
-		}
-		if(decoded.exp < Math.floor(Date.now() / 1000) ) {
-			res.status(401);
-			res.render('login', {});
-			return;
-		}
-		res.render('profile', {});
-	});
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- bactive/routes/profile.js
+++ bactive/routes/profile.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+function checkLogin(cookie, email) {
+	var cert = "C-UFRaksvPKhx1txJYFcut3QGxsafPmwCY6SCly3G6c";
+	if(cookie == null) {
+		console.log('no cookie');
+		return false; // return 401
+	}
+
+	try {
+		var decoded = jwt.verify(cookie, cert, {algorithms: ["HS256"]});
+
+		if(!('usr' in decoded) || decoded.usr != email) {
+			return false; // return 401
+		}
+	} catch (error) {
+		return false;
+	}
+
+	return true;
+}
 
 /* GET user information. */
 router.get('/:userid', function(req, res, next) {
@@ -13,6 +34,11 @@ router.get('/:userid', function(req, res, next) {
 					res.status(404).send("404: userId not found");
 				} else {
 					user = results[0]; // should only be one match
+
+					if (!checkLogin(req.cookies.jwt, user.email)) {
+						res.status(401).redirect('/login');
+						return;
+					}
 
 					res.render('profile', {
 					 	userId: userId,
