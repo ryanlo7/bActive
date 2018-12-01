1bc707931c10b5c9c4d1a5962d5a580bc339f8a6
elkong <erickongl@gmail.com>
2018-12-01 00:09:43 -0800

    Nicer login/register pages!

--- bactive/app.js
+++ bactive/app.js
@@ -24,7 +24,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const url = 'mongodb://localhost:27017/';
 const dbName = 'bActiveServer'; // database name is 'bActiveServer'
-MongoClient.connect(url, 
+MongoClient.connect(url,
 	function(err, client) {
 		assert.equal(null, err);
 		console.log("Connected successfully to server");
