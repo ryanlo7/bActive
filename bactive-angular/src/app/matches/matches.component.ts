81b51ca109cc10a6dd86ac3e484c181d36d16163
Ryan Lo <ryanlo7@ucla.edu>
2018-12-02 00:22:01 -0800

    start matches component

--- bactive-angular/src/app/matches/matches.component.ts
+++ bactive-angular/src/app/matches/matches.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { User, UserService } from '../user.service';
 
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.css']
 })
+
+// Source: CS144 with Professor Cho, Project 4 JWT code
+function parseJWT(token) 
+{
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    return JSON.parse(atob(base64));
+}
+
 export class MatchesComponent implements OnInit {
+	userId: number;
 
-  constructor() { }
+	constructor(private userService: UserService) {
+		this.userId = parseJWT(document.cookie).userId;
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.runMatch();
+	}
+
+	runMatch(): void {
+		
+	}
 
 }
